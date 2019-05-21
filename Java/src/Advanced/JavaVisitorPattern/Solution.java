5674bc8c4c4f3a79559905ffe3178ec08f969274
BinSin <binyong318@gmail.com>
2019-05-21 22:00:07 +0900

    visitor pattern

--- Java/src/Advanced/JavaVisitorPattern/Solution.java
+++ Java/src/Advanced/JavaVisitorPattern/Solution.java
@@ -1,17 +1,18 @@
 package Advanced.JavaVisitorPattern;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
 enum Color {
     RED, GREEN
 }
 
+// 데이터 인터페이스 (accept 함수 제공)
 abstract class Tree {
 
     private int value;
@@ -39,6 +40,7 @@ abstract class Tree {
     public abstract void accept(TreeVis visitor);
 }
 
+// 데이터 클래스 (visitor에게 자신을 던져줌으로써 끝)
 class TreeNode extends Tree {
 
     private ArrayList<Tree> children = new ArrayList<>();
@@ -60,6 +62,7 @@ class TreeNode extends Tree {
     }
 }
 
+//데이터 클래스 (visitor에게 자신을 던져줌으로써 끝)
 class TreeLeaf extends Tree {
 
     public TreeLeaf(int value, Color color, int depth) {
@@ -71,6 +74,7 @@ class TreeLeaf extends Tree {
     }
 }
 
+// visitor 인터페이스
 abstract class TreeVis
 {
     public abstract int getResult();
@@ -79,6 +83,7 @@ abstract class TreeVis
 
 }
 
+// visitor 구현체 1 (알고리즘)
 class SumInLeavesVisitor extends TreeVis {
 	
 	private int result = 0;
@@ -95,67 +100,67 @@ class SumInLeavesVisitor extends TreeVis {
     }
 }
 
+//visitor 구현체 2 (알고리즘)
 class ProductOfRedNodesVisitor extends TreeVis {
 	
-	private int result = 1;
+	private long result = 1; // long 으로 해야 result 제대로 나온다.
 	private final int M = 1000000007;
 	
     public int getResult() {
-    	return result;
+    	return (int) result;
     }
 
     public void visitNode(TreeNode node) {
     	if(node.getColor().equals(Color.RED))
-    		result *= node.getValue() % M;
+    		result = (result * node.getValue()) % M;
     }
 
     public void visitLeaf(TreeLeaf leaf) {
     	if(leaf.getColor().equals(Color.RED))
-    		result *= leaf.getValue() % M;
+    		result = (result * leaf.getValue()) % M;
     }
 }
 
+//visitor 구현체 3 (알고리즘)
 class FancyVisitor extends TreeVis {
 	
-	private int result = 0;
+	private int nonLeafEvenDepthSum = 0;
+    private int greenLeavesSum = 0;
 	
     public int getResult() {
-    	return Math.abs(result);
+    	return Math.abs(nonLeafEvenDepthSum - greenLeavesSum);
     }
 
     public void visitNode(TreeNode node) {
     	if(node.getDepth()%2 == 0)
-			result += node.getValue();
+    		nonLeafEvenDepthSum += node.getValue();
     }
 
     public void visitLeaf(TreeLeaf leaf) {
     	if(leaf.getColor().equals(Color.GREEN))
-    		result -= leaf.getValue();
+    		greenLeavesSum += leaf.getValue();
     }
 }
 
 public class Solution {
+	private static int[] values;
+	private static Color[] colors;
+	private static Map<Integer, Set<Integer>> nodesMap;
 	
-    public static Tree solve() throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static Tree solve() {
+    	Scanner sc = new Scanner(System.in);
 		
-		int n = Integer.parseInt(br.readLine());
-		int[] values = new int[n];
-		Color[] colors = new Color[n];
-		HashMap<Integer, LinkedList<Integer>> nodesMap = new HashMap<>();
+		int n = sc.nextInt();
+		values = new int[n];
+		colors = new Color[n];
+		nodesMap = new HashMap<>(n);
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<n; i++) {
-			values[i] = Integer.parseInt(st.nextToken());
+			values[i] = sc.nextInt();
 		}
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<n; i++) {
-			switch(Integer.parseInt(st.nextToken())) {
-			case 0 : colors[i] = Color.RED; break;
-			case 1 : colors[i] = Color.GREEN; break;
-			}
+			colors[i] = sc.nextInt() == 0 ? Color.RED : Color.GREEN;
 		}
     	
 		Tree rootNode;
@@ -164,24 +169,63 @@ public class Solution {
 		}
 		else {
 			for(int i=0; i<n-1; i++) {
-				st = new StringTokenizer(br.readLine());
-				int u = Integer.parseInt(st.nextToken());
-				int v = Integer.parseInt(st.nextToken());
-				LinkedList<Integer> edges = nodesMap.get(u);
-				if(edges == null) {
-					edges = new LinkedList<>();
+				int u = sc.nextInt();
+				int v = sc.nextInt();
+				
+				Set<Integer> uEdges = nodesMap.get(u);
+				if(uEdges == null) {
+					uEdges = new HashSet<>();
 				}
-				edges.add(v);
-				nodesMap.put(v, edges);
+				uEdges.add(v);
+				nodesMap.put(u, uEdges);
+				
+				Set<Integer> vEdges = nodesMap.get(v);
+				if(vEdges == null) {
+					vEdges = new HashSet<>();
+				}
+				vEdges.add(u);
+				nodesMap.put(v, vEdges);
 			}
 			
 			rootNode = new TreeNode(values[0], colors[0], 0);
-			LinkedList<Integer> 
+			Set<Integer> rootEdges = nodesMap.get(1);
+			Iterator<Integer> itr = rootEdges.iterator();
+			// 부모노드(1) 부터 시작
+			while(itr.hasNext()) {
+				Integer nodeIdentifier = itr.next();
+				// 부모노드의 자식노드를 제거 (시작!!)
+				nodesMap.get(nodeIdentifier).remove(1);
+				// 자식노드가 자식이 없으면 리프에 추가 아니면 노드에 추가
+				createEdge(rootNode, nodeIdentifier);
+			}
 		}
 		
-    	return null;
+		return rootNode;
+	}
+    
+    private static void createEdge(Tree parentNode, Integer nodeIdentifier) {
+    	Set<Integer> nodeEdges = nodesMap.get(nodeIdentifier);
+    	boolean hasChild = false;
+    	if(nodeEdges != null && !nodeEdges.isEmpty())
+    		hasChild = true;
+    	
+    	if(hasChild) { // 자식이 있으면 노드에 넣는다
+    		TreeNode node = new TreeNode(values[nodeIdentifier-1], colors[nodeIdentifier-1], parentNode.getDepth()+1);
+    		((TreeNode)parentNode).addChild(node); // 자식이 있으므로 노드에 추가
+    		Iterator<Integer> nodeIterator = nodeEdges.iterator();
+    		while(nodeIterator.hasNext()) { // 자식이 없을 때까지 계속 반복
+    			Integer neighborNodeIdentifier = nodeIterator.next();
+    			nodesMap.get(neighborNodeIdentifier).remove(nodeIdentifier); // 자식 노드 제거
+    			
+    			createEdge(node, neighborNodeIdentifier);
+    		}
+    	}
+    	else { // 자식이 없으면 리프에 넣는다 (끝!!)
+    		TreeLeaf leaf = new TreeLeaf(values[nodeIdentifier-1], colors[nodeIdentifier-1], parentNode.getDepth()+1);
+    		((TreeNode)parentNode).addChild(leaf);
+    	}
+    	
     }
-
 
     public static void main(String[] args) {
       	Tree root = solve();
