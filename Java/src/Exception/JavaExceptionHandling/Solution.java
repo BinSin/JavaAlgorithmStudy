19c5254de03994588ec6c0439df6d3012b52cb67
BinSin <binyong318@gmail.com>
2019-06-02 15:19:50 +0900

    try/catch

---
+++ Java/src/Exception/JavaExceptionHandling/Solution.java
@@ -0,0 +1,21 @@
+package Exception.JavaExceptionHandling;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		try {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			System.out.println(a / b);
+		} catch (ArithmeticException e) {
+			System.out.println(e);
+		} catch (InputMismatchException e) {
+			System.out.println(e.getClass().getName());
+		}
+	}
+}
